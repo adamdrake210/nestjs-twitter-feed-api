2cd970cf3e59f457e471d4f209d0d76e7b3c76d6
Adam Drake <addrake@ebay.com>
2022-03-05 17:10:38 +0100

    jwt implementation

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,21 @@
-import { Controller, Request, Post, UseGuards } from '@nestjs/common';
+import { Controller, Request, Post, UseGuards, Get } from '@nestjs/common';
+import { AuthService } from './auth/auth.service';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
 import { LocalAuthGuard } from './auth/local-auth.guard';
 
 @Controller()
 export class AppController {
+  constructor(private authService: AuthService) {}
+
   @UseGuards(LocalAuthGuard)
   @Post('auth/login')
   async login(@Request() req) {
+    return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
     return req.user;
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -6,19 +6,6 @@ import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
-  // private readonly users = [
-  //   {
-  //     userId: 1,
-  //     username: 'john',
-  //     password: 'changeme',
-  //   },
-  //   {
-  //     userId: 2,
-  //     username: 'maria',
-  //     password: 'guess',
-  //   },
-  // ];
-
   constructor(
     @InjectRepository(User)
     private readonly userRepository: Repository<User>,
