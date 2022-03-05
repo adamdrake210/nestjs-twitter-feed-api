6d9a77a2464d15bef1bb75469f762f6e4b036b79
Adam Drake <adamgedrake@gmail.com>
2022-03-05 17:11:35 +0100

    Authentication Implementation (#1)
    * authentication local strategy
    
    * jwt implementation
    
    Co-authored-by: Adam Drake <addrake@ebay.com>

---
+++ src/users/users.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './entities/user.entity';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  async findOne(username: string) {
+    return this.userRepository.findOne({ username });
+  }
+
+  async create(createUserDto: CreateUserDto) {
+    const newUser = await this.userRepository.create(createUserDto);
+    await this.userRepository.save(newUser);
+    return newUser;
+  }
+}
