ba5746999937368514ddc0032b5b82760e12a51f
Adam Drake <addrake@ebay.com>
2022-03-15 16:35:25 +0100

    clean up

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -59,7 +59,6 @@ export class AuthService {
   }
 
   async login(user: any) {
-    console.log('user: ', user);
     const payload = { email: user.email, sub: user.userId };
     return {
       access_token: this.jwtService.sign(payload),
--- src/tweeting/cron.service.ts
+++ src/tweeting/cron.service.ts
@@ -10,51 +10,51 @@ export class CronService {
     private readonly tweetInfoService: TweetInfoService,
   ) {}
 
-  // @Cron('*/8 * * * *')
-  // async cronLikeTweet() {
-  //   try {
-  //     console.log('🚀 ~ Running the Like Job');
-  //     const tweetInfo = await this.tweetInfoService.findOne('1');
-  //     const { tweettopics } = tweetInfo;
-  //     const chosenTopic =
-  //       tweettopics[Math.floor(Math.random() * tweettopics.length)];
-  //     this.tweetingService.getAndLikeTweet(chosenTopic);
-  //   } catch (error) {
-  //     throw new NotFoundException(
-  //       `Something went wrong liking a tweet: ${error}`,
-  //     );
-  //   }
-  // }
+  @Cron('*/8 * * * *')
+  async cronLikeTweet() {
+    try {
+      console.log('🚀 ~ Running the Like Job');
+      const tweetInfo = await this.tweetInfoService.findOne('1');
+      const { tweettopics } = tweetInfo;
+      const chosenTopic =
+        tweettopics[Math.floor(Math.random() * tweettopics.length)];
+      this.tweetingService.getAndLikeTweet(chosenTopic);
+    } catch (error) {
+      throw new NotFoundException(
+        `Something went wrong liking a tweet: ${error}`,
+      );
+    }
+  }
 
-  // @Cron('*/11 * * * *')
-  // async cronRetweet() {
-  //   try {
-  //     console.log('🚀 ~ Running the Retweet Job');
-  //     const tweetInfo = await this.tweetInfoService.findOne('1');
-  //     const { tweettopics } = tweetInfo;
-  //     const chosenTopic =
-  //       tweettopics[Math.floor(Math.random() * tweettopics.length)];
-  //     this.tweetingService.getAndRetweet(chosenTopic);
-  //   } catch (error) {
-  //     throw new NotFoundException(
-  //       `Something went wrong retweeting a tweet: ${error}`,
-  //     );
-  //   }
-  // }
+  @Cron('*/11 * * * *')
+  async cronRetweet() {
+    try {
+      console.log('🚀 ~ Running the Retweet Job');
+      const tweetInfo = await this.tweetInfoService.findOne('1');
+      const { tweettopics } = tweetInfo;
+      const chosenTopic =
+        tweettopics[Math.floor(Math.random() * tweettopics.length)];
+      this.tweetingService.getAndRetweet(chosenTopic);
+    } catch (error) {
+      throw new NotFoundException(
+        `Something went wrong retweeting a tweet: ${error}`,
+      );
+    }
+  }
 
-  // @Cron('* * * * *')
-  // async cronCreateTweet() {
-  //   try {
-  //     console.log('🚀 ~ Running the Create Tweet Job');
-  //     const tweetInfo = await this.tweetInfoService.findOne('1');
-  //     const { tweettopics } = tweetInfo;
-  //     const chosenTopic =
-  //       tweettopics[Math.floor(Math.random() * tweettopics.length)];
-  //     this.tweetingService.getAndWriteTweet(chosenTopic);
-  //   } catch (error) {
-  //     throw new NotFoundException(
-  //       `Something went wrong creating a tweet: ${error}`,
-  //     );
-  //   }
-  // }
+  @Cron('* * * * *')
+  async cronCreateTweet() {
+    try {
+      console.log('🚀 ~ Running the Create Tweet Job');
+      const tweetInfo = await this.tweetInfoService.findOne('1');
+      const { tweettopics } = tweetInfo;
+      const chosenTopic =
+        tweettopics[Math.floor(Math.random() * tweettopics.length)];
+      this.tweetingService.getAndWriteTweet(chosenTopic);
+    } catch (error) {
+      throw new NotFoundException(
+        `Something went wrong creating a tweet: ${error}`,
+      );
+    }
+  }
 }
