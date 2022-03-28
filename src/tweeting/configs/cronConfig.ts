f6ba59bdf737fdfc9e74c53a22ed1734a6073f8f
Adam Drake <addrake@ebay.com>
2022-03-28 16:39:39 +0200

    feat: better text in tweet

--- src/tweeting/configs/cronConfig.ts
+++ src/tweeting/configs/cronConfig.ts
@@ -1,24 +1,22 @@
-const isDev = process.env.NODE_ENV === 'development';
-
 export const cronConfig = {
   likingTweets: {
     schedule: '*/28 * * * *',
-    isRunning: !isDev && true,
+    isRunning: true,
   },
   retweetingTweets: {
     schedule: '*/23 * * * *',
-    isRunning: !isDev && true,
+    isRunning: true,
   },
   creatingTweets: {
     schedule: '*/35 * * * *',
-    isRunning: !isDev && false,
+    isRunning: false,
   },
   creatingOpenAiTweets: {
     schedule: '*/35 * * * *',
-    isRunning: !isDev && true,
+    isRunning: true,
   },
   creatingDevToTweets: {
     schedule: '*/32 * * * *',
-    isRunning: !isDev && true,
+    isRunning: true,
   },
 };
