aa8adc632061c5b8a34291838007ddca287ffb5c
Adam Drake <addrake@ebay.com>
2022-04-19 12:05:30 +0200

    update to cron

--- src/tweeting/cron.service.ts
+++ src/tweeting/cron.service.ts
@@ -50,55 +50,55 @@ export class CronService {
     }
   }
 
-  @Cron(cronConfig.creatingTweets.schedule)
-  async cronCreateTweet() {
-    if (cronConfig.creatingTweets.isRunning) {
-      try {
-        console.log('🚀 ~ Running the Create Tweet Job');
-        const tweetInfo = await this.tweetInfoService.findOne('1');
-        const { tweettopics } = tweetInfo;
-        const chosenTopic = randomiser(tweettopics);
-        this.tweetingService.getAndWriteTweet(chosenTopic);
-      } catch (error) {
-        throw new NotFoundException(
-          `Something went wrong creating a tweet: ${error}`,
-        );
-      }
-    }
-  }
+  // @Cron(cronConfig.creatingTweets.schedule)
+  // async cronCreateTweet() {
+  //   if (cronConfig.creatingTweets.isRunning) {
+  //     try {
+  //       console.log('🚀 ~ Running the Create Tweet Job');
+  //       const tweetInfo = await this.tweetInfoService.findOne('1');
+  //       const { tweettopics } = tweetInfo;
+  //       const chosenTopic = randomiser(tweettopics);
+  //       this.tweetingService.getAndWriteTweet(chosenTopic);
+  //     } catch (error) {
+  //       throw new NotFoundException(
+  //         `Something went wrong creating a tweet: ${error}`,
+  //       );
+  //     }
+  //   }
+  // }
 
-  @Cron(cronConfig.creatingOpenAiTweets.schedule)
-  async cronCreateOpenAiTweet() {
-    if (cronConfig.creatingOpenAiTweets.isRunning) {
-      try {
-        console.log('🚀 ~ Running the Create Tweet via OpenAI');
-        const tweetInfo = await this.tweetInfoService.findOne('1');
-        const { tweetquestions, tweettopics } = tweetInfo;
-        const chosenTopic = randomiser(tweettopics);
-        const chosenQuestion = randomiser(tweetquestions);
-        this.openaiService.createOpenAiTweet(chosenQuestion + chosenTopic);
-      } catch (error) {
-        throw new NotFoundException(
-          `Something went wrong creating a tweet for openai: ${error}`,
-        );
-      }
-    }
-  }
+  // @Cron(cronConfig.creatingOpenAiTweets.schedule)
+  // async cronCreateOpenAiTweet() {
+  //   if (cronConfig.creatingOpenAiTweets.isRunning) {
+  //     try {
+  //       console.log('🚀 ~ Running the Create Tweet via OpenAI');
+  //       const tweetInfo = await this.tweetInfoService.findOne('1');
+  //       const { tweetquestions, tweettopics } = tweetInfo;
+  //       const chosenTopic = randomiser(tweettopics);
+  //       const chosenQuestion = randomiser(tweetquestions);
+  //       this.openaiService.createOpenAiTweet(chosenQuestion + chosenTopic);
+  //     } catch (error) {
+  //       throw new NotFoundException(
+  //         `Something went wrong creating a tweet for openai: ${error}`,
+  //       );
+  //     }
+  //   }
+  // }
 
-  @Cron(cronConfig.creatingDevToTweets.schedule)
-  async cronCreateDevToTweet() {
-    if (cronConfig.creatingDevToTweets.isRunning) {
-      try {
-        console.log('🚀 ~ Running the Create Article Tweet via DevTo');
-        const tweetInfo = await this.tweetInfoService.findOne('1');
-        const { tweettopics } = tweetInfo;
-        const chosenTopic = randomiser(tweettopics);
-        this.devtoService.createDevToTweet(chosenTopic);
-      } catch (error) {
-        throw new NotFoundException(
-          `Something went wrong creating an article tweet for devto: ${error}`,
-        );
-      }
-    }
-  }
+  // @Cron(cronConfig.creatingDevToTweets.schedule)
+  // async cronCreateDevToTweet() {
+  //   if (cronConfig.creatingDevToTweets.isRunning) {
+  //     try {
+  //       console.log('🚀 ~ Running the Create Article Tweet via DevTo');
+  //       const tweetInfo = await this.tweetInfoService.findOne('1');
+  //       const { tweettopics } = tweetInfo;
+  //       const chosenTopic = randomiser(tweettopics);
+  //       this.devtoService.createDevToTweet(chosenTopic);
+  //     } catch (error) {
+  //       throw new NotFoundException(
+  //         `Something went wrong creating an article tweet for devto: ${error}`,
+  //       );
+  //     }
+  //   }
+  // }
 }
